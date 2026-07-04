5692043a968d601727f9bae2086736b0dca8931c
Mark Smalley <mark@smalley.my>
2026-07-04 07:46:48 +0100

    Add Components and RPGs filter pills to homepage

--- js/build.mjs
+++ js/build.mjs
@@ -634,6 +634,8 @@ function buildLanding() {
       <button class="filter-pill filter-pill--mod" data-filter="mod">Mods</button>
       <button class="filter-pill filter-pill--platform" data-filter="platform">Platforms</button>
       <button class="filter-pill filter-pill--classic" data-filter="classic">Classics</button>
+      <button class="filter-pill filter-pill--component" data-filter="component">Components</button>
+      <button class="filter-pill filter-pill--rpg" data-filter="rpg">RPGs</button>
     </div>
     <div class="search-bar">
       <svg class="search-icon" viewBox="0 0 20 20" fill="currentColor" width="16" height="16"><path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd"/></svg>
